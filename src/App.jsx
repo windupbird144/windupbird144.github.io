90b5dbbf8a0b85c6d7d97c98cc347c0e07f0b74a
Diyar Oktay <diyar.oktay@outlook.com>
2022-08-17 21:12:03 +0200

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
@@ -12,16 +11,16 @@ function App() {
     pristine: true,
   };
 
-  const [deck, setDeck] = useState(initialDeck);
+  const [state, setState] = useState(initialDeck);
 
   const reveal = (e) => {
     const i = values.indexOf(e);
-    if (deck.deck[i] < 0) return; // cannot play any more of this card
-    deck.deck[i]--;
+    if (state.deck[i] <= 0) return; // cannot play any more of this card
+    state.deck[i]--;
 
-    const cardsLower = deck.deck.slice(0, i).reduce((a, b) => a + b, 0);
-    const cardsHigher = deck.deck.slice(i + 1).reduce((a, b) => a + b, 0);
-    const cardsTotal = deck.deck.reduce((a, b) => a + b);
+    const cardsLower = state.deck.slice(0, i).reduce((a, b) => a + b, 0);
+    const cardsHigher = state.deck.slice(i + 1).reduce((a, b) => a + b, 0);
+    const cardsTotal = state.deck.reduce((a, b) => a + b);
 
     // evavu fails if we have a higher card
     const evavuFail = cardsHigher / cardsTotal;
@@ -30,37 +29,40 @@ function App() {
     let [bestChoice, failPercent] =
       evavuFail < tyranuFail ? ["Evavu", evavuFail] : ["Tyranu", tyranuFail];
 
-    setDeck({
+    setState({
       open: e,
-      deck: deck.deck,
+      deck: state.deck,
       bestChoice,
       failPercent,
       successPercent: 1 - failPercent,
       pristine: false,
+      complete: state.deck.reduce((a,b) => a+b) === 0
     });
   };
 
-  const reset = () => setDeck(initialDeck);
+  const reset = () => setState(initialDeck);
 
   const format = (d) => (d * 100).toFixed();
 
   return (
     <div className="counter">
-      {deck.pristine ? (
-        "Click the first card to begin"
-      ) : (
-        <>
+      {
+        (function() {
+          if (state.pristine) return "Click the first card to begin"
+          if (state.complete) return "Game won!"
+          return <>
           <span>You should pick</span>
-          <span className="choice">{deck.bestChoice}</span>
+          <span className="choice">{state.bestChoice}</span>
           <span className="odds">
-            {format(deck.successPercent)}% chance of success
+            {format(state.successPercent)}% chance of success
           </span>
-          <span>On top: {deck.open}</span>
-        </>
-      )}
+          <span>On top: {state.open}</span>
+        </>    
+        }())
+      }
       <div className="buttons">
-        {values.map((e) => (
-          <button key={e} onClick={() => reveal(e)}>
+        {values.map((e,i) => (
+          <button key={e} onClick={() => reveal(e)} disabled={state.deck[i] === 0}>
             {e}
           </button>
         ))}
@@ -70,7 +72,7 @@ function App() {
       </button>
       <details>
         <summary>Stats</summary>
-        {JSON.stringify(deck, null, 4)}
+        {JSON.stringify(state, null, 4)}
       </details>
     </div>
   );

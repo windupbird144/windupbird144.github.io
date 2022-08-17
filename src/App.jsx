7f50438cb7286de85a7e28a0547dd6d4a82a6ec3
Diyar Oktay <diyar.oktay@outlook.com>
2022-08-17 21:50:52 +0200

    Add keyboard support

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 
 function App() {
+
   const initialDeck = {
     open: null,
     deck: values.map(() => 4),
@@ -36,46 +37,101 @@ function App() {
       failPercent,
       successPercent: 1 - failPercent,
       pristine: false,
-      complete: state.deck.reduce((a,b) => a+b) === 0
+      remaining: state.deck.reduce((a,b) => a+b),
+      get isCompleted() {
+        return this.remaining == 0
+      }
     });
   };
+
+
+  useEventListener("keyup", (e) => {
+    // A number key e.g. "2" must be parsed where as a letter key such
+    // as "k" for king must be converted to uppercase
+    let key = parseInt(e.key) || e.key.toUpperCase()
+    if (key === "0") key = 10
+    if (values.includes(key)) reveal(key)
+  })
+
 
   const reset = () => setState(initialDeck);
 
   const format = (d) => (d * 100).toFixed();
 
   return (
-    <div className="counter">
-      {
-        (function() {
-          if (state.pristine) return "Click the first card to begin"
-          if (state.complete) return "Game won!"
-          return <>
-          <span>You should pick</span>
-          <span className="choice">{state.bestChoice}</span>
-          <span className="odds">
-            {format(state.successPercent)}% chance of success
-          </span>
-          <span>On top: {state.open}</span>
-        </>    
-        }())
-      }
-      <div className="buttons">
-        {values.map((e,i) => (
-          <button key={e} onClick={() => reveal(e)} disabled={state.deck[i] === 0}>
-            {e}
-          </button>
-        ))}
+    <div id="app">      
+      <div className="counter">
+        {
+          (function() {
+            if (state.pristine) return "Click the first card to begin"
+            if (state.isCompleted) return "Game won!"
+            return <>
+            <span>You should pick</span>
+            <span className="choice">{state.bestChoice}</span>
+            <span className="odds">
+              {format(state.successPercent)}% chance of success
+            </span>
+            <span>On top: {state.open}</span>
+            <span>Remaining: {state.remaining}</span>
+          </>    
+          }())
+        }
+        <div className="buttons">
+          {values.map((e,i) => (
+            <button key={e} onClick={() => reveal(e)} disabled={state.deck[i] === 0}>
+              {e}
+            </button>
+          ))}
+        </div>
+        <button className="start-over" onClick={reset}>
+          Start over
+        </button>
+        <details>
+          <summary>Stats</summary>
+          {JSON.stringify(state, null, 4)}
+        </details>
       </div>
-      <button className="start-over" onClick={reset}>
-        Start over
-      </button>
-      <details>
-        <summary>Stats</summary>
-        {JSON.stringify(state, null, 4)}
-      </details>
+      <div>Tip: you can use your keyboard to reveal cards. Key 0 = 10</div>
     </div>
+
   );
 }
 
 export default App;
+
+
+// https://usehooks.com/useEventListener/
+// Hook
+function useEventListener(eventName, handler, element = window) {
+  // Create a ref that stores handler
+  const savedHandler = useRef();
+
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler ...
+  // ... without us needing to pass it in effect deps array ...
+  // ... and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(
+    () => {
+      // Make sure element supports addEventListener
+      // On
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+
+      // Create event listener that calls handler function stored in ref
+      const eventListener = (event) => savedHandler.current(event);
+
+      // Add event listener
+      element.addEventListener(eventName, eventListener);
+
+      // Remove event listener on cleanup
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element] // Re-run if eventName or element changes
+  );
+}
